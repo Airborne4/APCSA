6d8b1c609376177cf3419999f57e9b5b4abc23a8
Airborne4 <nrcornick@gmail.com>
2020-02-27 11:09:59 -0800

    Updated Deck Class
    cool

--- src/ElevensLab/Deck.java
+++ src/ElevensLab/Deck.java
@@ -9,21 +9,30 @@ import java.util.Arrays;
 import java.util.Collections;
 
 //make a Deck class
-
+public class Deck{
 	public static final int NUMCARDS = 52;
 	public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
 	
 	private List<Card> cards;
 	private int top;
-
+	
+	cards = new ArrayList<Card>();
    //make a Deck constructor
+	public Deck() {
    	//refer cards to new ArrayList
    	//set top to the top of the deck 51
-   	
+   	top = 51;
    	//loop through all suits
    		//loop through all faces 1 to 13
    			//add a new TwentyOneCard to the deck
+for (int s = 0; s < SUITS.length; s++) {
+	for(int f = 1; f <= 13; f++) {
+		cards.add(new Card(SUITS[s] , f));
+	}
+}
 
+}
+}
    
    //make a dealCard() method that returns the top card
    
