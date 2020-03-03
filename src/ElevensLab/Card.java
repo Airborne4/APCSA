e868cbadff42a13c67d924d1c99c8ee15ed2f51c
Airborne4 <nrcornick@gmail.com>
2020-03-03 11:01:31 -0800

    CardsAndDecks
    Activity 1 and 2

--- src/ElevensLab/Card.java
+++ src/ElevensLab/Card.java
@@ -3,26 +3,93 @@
 //Name -
 package ElevensLab;
 
-public class Card
-{
-	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
-			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+public class Card {
+	public static final String FACES[] = { "ZERO", "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+			"NINE", "TEN", "JACK", "QUEEN", "KING" };
 
-	//instance variables
-		//String suit
-		//int face
+	// instance variables
+	String suit = "";
+	String rank = "";
+	int face = 0;
+	int pointValue = 0;
 
-  	//constructors
+	// constructors
+	public Card() {
 
+	}
+
+	public Card(String suit, int face) {
+		this.suit = suit;
+		this.face = face;
+	}
+
+	public Card(String cardRank, String cardSuit, int cardPointValue) {
+		this.rank = cardRank;
+		this.suit = cardSuit;
+		this.pointValue = cardPointValue;
+	}
 
 	// modifiers
-		//set methods
+	// set methods
+	
+	//auto-generated (super cool)
+	public void setFace(int face) {
+		this.face = face;
+	}
 
+	public int getPointValue() {
+		return pointValue;
+	}
 
-  	//accessors
-		//get methods
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
 
+	public String getRank() {
+		return rank;
+	}
 
-  	//toString
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
 
- }
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	// accessors
+	// get methods
+	public int getFace() {
+		return face;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public boolean matches(Card otherCard) {
+		boolean isEqual = false;
+		if (getFace() != 0) {
+			if (this.getSuit().equals(otherCard.getSuit()) && this.getFace() == otherCard.getFace()) {
+				isEqual = true;
+			}
+		} else {
+			if (this.getSuit().equals(otherCard.getSuit()) && this.getRank().equals(otherCard.getRank())
+					&& this.getPointValue() == otherCard.getPointValue()) {
+				isEqual = true;
+			}
+		}
+
+		return isEqual;
+	}
+
+	// toString
+	public String toString() {
+		if (pointValue == 0) {
+			return "" + FACES[face] + " of " + getSuit();
+		} else {
+			return rank + " of " + suit + " (Point Value of " + pointValue + ")";
+		}
+	}
+
+}
--- src/ElevensLab/Deck.java
+++ src/ElevensLab/Deck.java
@@ -1,41 +1,88 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
+
 package ElevensLab;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 //make a Deck class
-public class Deck{
+public class Deck {
 	public static final int NUMCARDS = 52;
 	public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
-	
+
 	private List<Card> cards;
 	private int top;
-	
-	cards = new ArrayList<Card>();
-   //make a Deck constructor
-	public Deck() {
-   	//refer cards to new ArrayList
-   	//set top to the top of the deck 51
-   	top = 51;
-   	//loop through all suits
-   		//loop through all faces 1 to 13
-   			//add a new TwentyOneCard to the deck
-for (int s = 0; s < SUITS.length; s++) {
-	for(int f = 1; f <= 13; f++) {
-		cards.add(new Card(SUITS[s] , f));
-	}
-}
+	private int size = 52;
 
+	// make a Deck constructor
+	// refer cards to new ArrayList
+	// set top to the top of the deck 51
+	public Deck() {
+		size = 52;
+		cards = new ArrayList<Card>();
+		top = 51;
+		for (String suit : SUITS) {
+			for (int face = 1; face < 14; face++) {
+				cards.add(new Card(suit, face));
+			}
+		}
+	}
+
+	public Deck(String[] rank, String[] suit, int[] values) {
+		size = suit.length * values.length;
+		cards = new ArrayList<Card>();
+		top = size - 1;
+		for (String suitType : suit) {
+			for (int i = 0; i < rank.length; i++) {
+
+				cards.add(new Card(rank[i], suitType, values[i]));
+
+			}
+		}
+	}
+
+	public boolean isEmpty() {
+		return top < 0;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public Card dealCard() {
+
+		System.out.println(!isEmpty());
+		if (!isEmpty()) {
+			size--;
+			top--;
+			return cards.get(top + 1);
+
+		} else {
+			return new Card();
+		}
+
+	}
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+		top = 51;
+		size = 52;
+	}
+
+	public void reset() {
+		size = 52;
+		top = 51;
+	}
+	// loop through all suits
+	// loop through all faces 1 to 13
+	// add a new TwentyOneCard to the deck
+
+	// make a dealCard() method that returns the top card
+
+	// write a shuffle() method
+	// use Colletions.shuffle
+	// reset the top card
 }
-}
-   
-   //make a dealCard() method that returns the top card
-   
-   //write a shuffle() method
-   	//use Colletions.shuffle
-   	//reset the top card 
