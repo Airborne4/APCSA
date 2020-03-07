8d62d48a66c0b4e9da5427b95aece7f01b1974b2
Airborne4 <nrcornick@gmail.com>
2020-03-06 21:41:07 -0800

    Finished Toy Store
    nice

--- src/Unit10/ToyStoreRunner.java
+++ src/Unit10/ToyStoreRunner.java
@@ -1,21 +1,19 @@
 // (c) A+ Computer Science
 // www.apluscompsci.com
 // Name -
-package Unit10; 
+package Unit10;
 
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import static java.lang.System.*;
 
-public class ToyStoreRunner
-{
-	public static void main( String args[] )
-	{
-		ToyStore sto = new ToyStore();
-		System.out.println( sto );
-		sto.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball" );
-		System.out.println( sto );	
-		System.out.println( "max == " + sto.getMostFrequentToy() );		
+public class ToyStoreRunner {
+	public static void main(String args[]) {
+		ToyStore32 sto = new ToyStore32();
+		System.out.println(sto);
+		sto.loadToys("sorry bat sorry sorry sorry train train teddy teddy ball ball");
+		System.out.println(sto);
+		System.out.println("max == " + sto.getMostFrequentToy());
 	}
 }
